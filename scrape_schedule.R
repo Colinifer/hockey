85f09c14dc477d41f97f9850ab2921187983d099
Colin <colinrwelsh@gmail.com>
2020-03-03 23:31:15 -0500

    schedule update

--- scrape_schedule.R
+++ scrape_schedule.R
@@ -1,8 +1,18 @@
-userYear = 2000
+userYear = 2019
+userDate <- Sys.Date()
+fschedule <- paste("schedule/", userYear, "schedule.csv", sep = "")
 
 if (userYear <= 2019) {
   schedule <- sc.scrape_schedule(start_date = paste(userYear, "-10-01", sep = ""), end_date = paste(userYear + 1, "-07-01", sep = ""), print_sched = TRUE)
   
-  write.csv(schedule, file = paste("schedule/", userYear, "schedule.csv", sep = ""), row.names = FALSE)
+  write.csv(schedule, file = fschedule, row.names = FALSE)
   userYear = userYear + 1
 }
+
+
+
+schedule <- read.csv(file = fschedule)
+tibble(schedule)
+
+today <- schedule %>% 
+  filter(game_date == paste(Sys.Date()))
