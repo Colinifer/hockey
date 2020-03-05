07c422ca20df3055de4afa175d5a005fc9869804
Colinifer <colinrwelsh@gmail.com>
2020-03-05 18:52:10 +0000

    scrape dataframe updates

--- pbp_scrape.R
+++ pbp_scrape.R
@@ -3,12 +3,21 @@ library(lubridate)
 library(tidyverse)
 
 source("EH_scrape_functions.R")
-fpbp_scrape <- paste("data/pbp/", userYear, "pbp.csv", sep = "")
+game_ids <- as.character(seq(2019020001, 2019020100, by = 1))
+x <- 1
 
 ## Scrape games
-pbp_scrape <- sc.scrape_pbp(games = as.character(seq(2019020001, 2019020100, by = 1)))
+pbp_scrape <- sc.scrape_pbp(games = game_ids[x])
 
-pbp_scrape %>% write.csv(file = fpbp_scrape, row.names = FALSE)
+game_info_df_new <-     pbp_scrape$game_info_df       ## game information data
+pbp_base_new <-         pbp_scrape$pbp_base           ## main play-by-play data
+pbp_extras_new <-       pbp_scrape$pbp_extras         ## extra play-by-play data
+player_shifts_new <-    pbp_scrape$player_shifts      ## full player shifts data
+player_periods_new <-   pbp_scrape$player_periods     ## player TOI sums per period
+roster_df_new <-        pbp_scrape$roster_df          ## roster data
+scratches_df_new <-     pbp_scrape$scratches_df       ## scratches data
+event_summary_df_new <- pbp_scrape$events_summary_df  ## event summary data
+scrape_report <-        pbp_scrape$report             ## scrape report
 
 
 ####################
@@ -17,11 +26,16 @@ pbp_scrape %>% write.csv(file = fpbp_scrape, row.names = FALSE)
 
 ####################
 
-playerSeason <- list.files(paste("data/players/", userYear, "/", sep = ""),
-                           pattern = "*.csv", full.names = TRUE) %>%
+game_ids <- as.character(seq(2019020001, 2019020100, by = 1))
+x <- 1
+
+sc.scrape_pbp(games = game_ids[x])
+
+pbp_season <- list.files(paste("data/pbp/", userYear, "/", sep = ""),
+                         pattern = "*.csv", full.names = TRUE) %>%
   lapply(read_csv) %>%
   bind_rows
 playerSeason
-write.csv(playerSeason, file = paste("data/season_total/", userYear, "players.csv", sep = ""), row.names=FALSE)
+write.csv(playerSeason, file = paste("data/pbp/", userYear, "pbp.csv", sep = ""), row.names=FALSE)
 
 ####################
