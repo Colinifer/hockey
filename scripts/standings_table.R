9f131fae21d36b09aa21b7a29210ae5ed9521387
Colin Welsh <colinrwelsh@gmail.com>
2021-04-21 23:49:54 -0400

    4.21 update

--- scripts/standings_table.R
+++ scripts/standings_table.R
@@ -1,10 +1,11 @@
 
-# standings <- 
-  nhlapi::nhl_standings() %>% 
-  # select(teamRecords) %>% 
-  unnest(cols = c(teamRecords)) %>% 
-  as_tibble() %>% 
-  identity() %>% 
+
+# standings <-
+nhlapi::nhl_standings(seasons = 2010) %>%
+  # select(teamRecords) %>%
+  unnest(cols = c(teamRecords)) %>%
+  as_tibble() %>%
+  identity() %>%
   select(
     division = division.name,
     team_name = team.name,
@@ -16,9 +17,19 @@
     goals_against = goalsAgainst,
     # everything(),
     NULL
-  ) %>% 
+  ) %>%
   mutate(
-    goal_differential = goals_for - goals_against
-  )
+    goal_differential = goals_for - goals_against,
+    off_scoring_rk = dense_rank(desc(goals_for)),
+    def_scoring_rk = dense_rank(goals_against),
+    index = (off_scoring_rk + def_scoring_rk) / 2,
+    NULL
+  ) %>% select(
+    -team_id,
+    -goals_for,
+    -goals_against,
+    -goal_differential ,
+    -def_scoring_rk,
+    -off_scoring_rk) %>% 
+  arrange(index)
 
-  
